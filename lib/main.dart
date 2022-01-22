19f84b90401b35746e82b7edc5127dfb5ca6971a
George Lee <george.p.lee.iii@gmail.com>
2022-01-22 10:21:29 +1000

    save

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+// you want to produce different outcomes of related classes under different conditions (i.e. discount, wholesale).
 void main() {
+  // set to receive information that you want from a special configuration.
   var cost = RetailPricingVisitor();
   var discount = DiscountPricingVisitor();
-
+  // set a list of objects that you want to visit.
   var workStuff = [Laptop(), Mobile(), Tablet()];
+  // visit each object in the list.
   for (var equipment in workStuff) {
+    // calls the specific gear (i.e. laptop, mobile, tablet) and triggers a specific method in the Visitor.
+    // (i.e. visitLaptop, visitMobile, visitTablet).
+    // this method handles the processing of that peice of gear.
     equipment.accept(cost);
   }
+
   print('Total cost: ${cost.totalPrice}');
 
   for (var equipment in workStuff) {
@@ -14,6 +21,7 @@ void main() {
   print('Total cost: ${discount.totalPrice}');
 }
 
+// different Pricings are modifiable by using different concrete implementations of the interface.
 class RetailPricingVisitor extends EquipmentVisitor {
   double _totalPrice = 0.0;
   double get totalPrice => _totalPrice;
@@ -34,6 +42,7 @@ class RetailPricingVisitor extends EquipmentVisitor {
   }
 }
 
+// sets the output of the conditions.
 class DiscountPricingVisitor extends EquipmentVisitor {
   double _totalPrice = 0.0;
   double get totalPrice => _totalPrice;
@@ -61,12 +70,17 @@ abstract class EquipmentVisitor {
   void visitLaptop(Laptop laptop);
 }
 
+// each peice of equipment has a method called accept that takes a visitor.
+// the accept method for each concrete class will call the appropriate visit method for the visitor.
+// this is possible because of polymorphism (i.e. extends),
+// which does not require a @oveerride for each method.
 abstract class Equipment {
   Equipment(this.name);
   String name;
 
   int? watt;
   late double retailPrice;
+
   double discountPrice() => retailPrice;
 
   void accept(visitor);
@@ -76,6 +90,9 @@ class Mobile extends Equipment {
   Mobile() : super('Mobile Phone') {
     retailPrice = 350.00;
   }
+  // it was not necessary to override discountPrice because of polymorphism (extends).
+  // @override
+  // double discountPrice() => retailPrice * 0.9;
 
   @override
   void accept(visitor) => visitor.visitMobile(this);
